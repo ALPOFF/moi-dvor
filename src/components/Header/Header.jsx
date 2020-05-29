8416ae3557192d14d7ecd89fd7119ebc3ba98a74
ALPOFF <leha.mms82@gmail.com>
2020-05-30 00:14:36 +0300

    d

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import './Header.scss'
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="header">
+            <NavLink className="navbar_item_container" to="/messages">
+                <div className="menutext">Сообщения</div>
+            </NavLink>
+
+            <NavLink className="navbar_item_container" to="/neighbors">
+                <div className="menutext">Соседи</div>
+            </NavLink>
         </div>
     )
 };

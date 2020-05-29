c2c7ee8c19bad30446d5b859507bc1871c625f24
ALPOFF <leha.mms82@gmail.com>
2020-05-29 21:47:11 +0300

    added custom test component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import CustomComp from "./components/CustomComp";
 
 const App = () => (
     <div>
       <h1>hi</h1>
+      <CustomComp />
     </div>
 );
 
---
+++ src/components/CustomComp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CustomComp = () => {
+    return (
+        <div><h1>hi from custom</h1></div>
+    )
+};
+
+export default CustomComp;

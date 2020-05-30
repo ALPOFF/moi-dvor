156abdd4fae56651185a137229ab42fcb58a8264
Ilyas Garipov <ilyasgaripov97@gmail.com>
2020-05-30 13:30:15 +0300

    Change header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,24 @@ import {NavLink} from "react-router-dom";
 const Header = () => {
     return (
         <div className="header">
-
-            <NavLink className="nav-item header-element" to="/messages">   
-                <div className="img-placeholder">
-                    <p>TT</p>
-                </div>
-                <div>Сообщения</div>
+            <NavLink className="nav-item header-element" to="/channels">  
+                <img className="inline-block" src="https://weboverhauls.github.io/demos/svg/checkmark.svg" width="30" alt=""/>
+                <div className="inline-block">Каналы</div>
+            </NavLink>
+            <NavLink className="nav-item header-element" to="/neighbors">  
+                <img className="inline-block" src="https://weboverhauls.github.io/demos/svg/checkmark.svg" width="30" alt=""/>
+                <div className="inline-block">Соседи</div>
+            </NavLink>
+            <NavLink className="nav-item header-element" to="/messages">  
+                <img className="inline-block" src="https://weboverhauls.github.io/demos/svg/checkmark.svg" width="30" alt=""/>
+                <div className="inline-block">Сообщения</div>
             </NavLink>
 
-            <NavLink className="nav-item header-element" to="/neighbors">
-                <div>Соседи</div>
+            <NavLink className="nav-item header-element" to="/search">  
+                <img className="inline-block" src="https://weboverhauls.github.io/demos/svg/checkmark.svg" width="30" alt=""/>
+                <div className="inline-block">Поиск</div>
             </NavLink>
+
         </div>
     )
 };
